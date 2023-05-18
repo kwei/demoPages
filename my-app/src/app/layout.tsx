fcf8bbf1ee8aed0d2354739731038782b0ae0320
kw_yeh <kw_yeh@cyberlink.com>
2023-05-18 15:48:31 +0800

    feat: meta

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -6,18 +6,30 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Example of IndexedDB',
   description: 'Using Next.js and React.js to implement a demo page for indexedDB.',
-  keywords: ['IndexedDB', 'Next.js', 'React.js'],
+  authors: [{ name: 'KW', url: 'kaiweiyeh2018@gmail.com' }],
+  creator: 'KW',
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true
+    }
+  },
+  metadataBase: new URL('https://indexed-db-example.vercel.app'),
   openGraph: {
-    url: 'https://indexed-db-example.vercel.app/indexedDB',
     title: 'Example of IndexedDB',
     siteName: 'Example of IndexedDB',
+    description: 'Using Next.js and React.js to implement a demo page for indexedDB.',
+    url: 'https://indexed-db-example.vercel.app/indexedDB',
     images: [
       {
         url: 'https://nextjs.org/og.png',
-        width: 800,
+        width: 600,
         height: 600,
       },
     ],
+    authors: ['KW'],
     locale: 'en-US',
     type: 'website'
   }
