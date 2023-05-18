194ebb4defd9b2bc31ec6ced3c0b0fffc18ad1e3
kw_yeh <kw_yeh@cyberlink.com>
2023-05-18 14:35:58 +0800

    test meta data

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -5,8 +5,19 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Example of IndexedDB',
-  description: 'Using Next.js to be the framework',
-  keywords: ['IndexedDB', 'Next.js']
+  description: 'Using Next.js and React.js to implement a demo page for indexedDB.',
+  keywords: ['IndexedDB', 'Next.js', 'React.js'],
+  url: 'https://indexed-db-example.vercel.app/indexedDB',
+    siteName: 'Example of IndexedDB',
+    images: [
+      {
+        url: 'https://nextjs.org/og.png',
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: 'en-US',
+    type: 'website',
 }
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
