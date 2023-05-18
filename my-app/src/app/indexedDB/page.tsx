1a2d76fe4afc10f6bb25490e9dd9b4597fc52912
kw_yeh <kw_yeh@cyberlink.com>
2023-05-18 18:06:48 +0800

    fix: dom hir

--- my-app/src/app/indexedDB/page.tsx
+++ my-app/src/app/indexedDB/page.tsx
@@ -27,14 +27,27 @@ export const metadata = {
 export default function Home() {
     
     return (
-      <main className="relative flex w-full h-full flex-row flex-wrap gap-5 p-4 md:p-8 ml:p-12">
-        <h1 className="w-full text-2xl font-bold">Simple Test for IndexedDB</h1>
-        <CreateStoreCard />
-        <DeleteStoreCard />
-        <AddDataCard />
-        <UpdateDataCard />
-        <DeleteDataCard />
-        <ActionCard />
-      </main>
+      <>
+        <main className="relative flex w-full h-full flex-row flex-wrap gap-5 p-4 md:p-8 ml:p-12">
+          <h1 className="w-full text-2xl font-bold">Simple Test for IndexedDB</h1>
+          <CreateStoreCard />
+          <DeleteStoreCard />
+          <AddDataCard />
+          <UpdateDataCard />
+          <DeleteDataCard />
+          <ActionCard />
+        </main>
+        <footer className='relative flex flex-col gap-4 p-8 bg-[#191919FF] text-[#F3F3F3FF]'>
+          <span>Copyright &copy; 2023 KW Yeh</span>
+          <div className='relative flex items-center gap-4'>
+            <a 
+              className='underline text-sm' 
+              href='https://github.com/kwei/indexedDBExample' 
+              target="_blank" 
+              rel="noreferrer noopener"
+            >View Source</a>
+          </div>
+        </footer>
+      </>
     )
   }
--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -23,17 +23,6 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      <footer className='relative flex flex-col gap-4 p-8 bg-[#191919FF] text-[#F3F3F3FF]'>
-        <span>Copyright &copy; 2023 KW Yeh</span>
-        <div className='relative flex items-center gap-4'>
-          <a 
-            className='underline text-sm' 
-            href='https://github.com/kwei/indexedDBExample' 
-            target="_blank" 
-            rel="noreferrer noopener"
-          >View Source</a>
-        </div>
-      </footer>
     </html>
   )
 }
